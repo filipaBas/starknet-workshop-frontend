ea3baa975682fbfee9032518f003b676849101be
BastosFilipa <filipa.s.bastos@gmail.com>
2024-09-08 19:22:28 +0100

    add mint

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -216,6 +216,7 @@ const Page: React.FC = () => {
 
     return "Send";
   };
+
   // Step 4 --> Write to a contract -- End
 
   return (
@@ -313,6 +314,48 @@ const Page: React.FC = () => {
       )}
       {/* --> Read total supply -- end */}
       {/* Step 4 --> Write to a contract -- Start */}
+
+      {/* Mint ERC20 Form */}
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-4 w-full max-w-md m-4 border-black border"
+      >
+        <h3 className="text-2xl font-bold mb-2">Mint ERC20</h3>
+        <label
+          htmlFor="amount"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Amount:
+        </label>
+        <input
+          type="number"
+          id="amount"
+          value={amount}
+          onChange={(event) => setAmount(event.target.valueAsNumber)}
+          className="block w-full px-3 py-2 text-sm leading-6 border-black focus:outline-none focus:border-yellow-300 black-border-p"
+        />
+        {writeData?.transaction_hash && (
+          <a
+            href={explorer.transaction(writeData?.transaction_hash)}
+            target="_blank"
+            className="text-blue-500 hover:text-blue-700 underline"
+            rel="noreferrer"
+          >
+            Check TX on {explorer.name}
+          </a>
+        )}
+        <div className="flex justify-center pt-4">
+          <button
+            type="submit"
+            className={`border border-black text-black font-regular py-2 px-4 ${
+              userAddress ? "bg-yellow-300 hover:bg-yellow-500" : "bg-white"
+            } `}
+            disabled={!userAddress}
+          >
+            {buttonContent()}
+          </button>
+        </div>
+      </form>
 
       {/* --> Transfer to a contract -- start */}
       <form
